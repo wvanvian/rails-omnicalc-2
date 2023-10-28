9ee8139f41fce549b61c1d5f3f35a5eebe8eaacf
wvanvian <146589485+wvanvian@users.noreply.github.com>
2023-10-28 21:25:49 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "addition", :action => "show_addition_form" })
 
-  get("/ad", { :controller => "addition", :action => "show_addition_form" })
-  get("/wizard_add", { :controller => "addition", :action => "add_these" })
+  get("/add", { :controller => "addition", :action => "show_addition_form" })
+  get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
-  get("", { :controller => "subtraction", :action => "subtract" })
+  get("/subtract", { :controller => "subtract", :action => "show_subtraction_form" })
+  get("/wizard_subtract", { :controller => "subtract", :action => "subtract_these" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+  get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
+  get("/wizardmultiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
-  get("/wizard_div", { :controller => "div", :action => "divide_these" })
+  get("/wizard_div", { :controller => "division", :action => "divide_these" })
 
 end
